ec72a1d67b8678d93f03db6636ea45eca17d100d
torikachi <a3601000er@gmail.com>
2025-11-23 14:54:47 +0700

    Updated

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,3 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "MUI Element Examples",
   description: "Ví dụ về các thành phần giao diện người dùng sử dụng Material-UI",
@@ -22,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
