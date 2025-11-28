7972af24135b725d3b7e40792529ba5fa2ccdac6
torikachi <a3601000er@gmail.com>
2025-11-29 00:57:40 +0700

    Updated

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,8 @@ export const metadata = {
   title: "MUI Element Examples",
   description: "Ví dụ về các thành phần giao diện người dùng sử dụng Material-UI",
   icons: {
-    icon: "/tkw/demo/images/image.png",
-    shortcut: "/tkw/demo/images/image.png",
+    icon: "/demo/images/image.png",
+    shortcut: "/demo/images/image.png",
   },
 };
 
