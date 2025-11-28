4f9d031e7512f6e6e1f171bf3f43c5ef32b72a09
torikachi <a3601000er@gmail.com>
2025-11-29 00:47:27 +0700

    Updated

--- app/demo/layout.js
+++ app/demo/layout.js
@@ -1,15 +1,15 @@
 export const metadata = {
-  title: "GreenFarm",
-  description: "Website bán nông sản - rau củ sạch",
+  title: 'GreenFarm',
+  description: 'Website bán nông sản - rau củ sạch',
   icons: {
-    icon: "/demo/images/image.png",
-    shortcut: "/demo/images/image.png",
+    icon: '/demo/images/image.png',
+    shortcut: '/demo/images/image.png',
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         {children}
       </body>
